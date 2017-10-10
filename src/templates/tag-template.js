8c4b16fa40e149890c51f125067296f7efbfdd53
aberkow <berkowitz.clarinet@gmail.com>
2017-10-09 22:13:53 -0400

    working on filtering tags

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -3,8 +3,20 @@ import Link from 'gatsby-link';
 
 export default class TagRoute extends Component {
   render() {
+    const tagToFind = this.props.pathContext.tag;
     const posts = this.props.data.allMarkdownRemark.edges;
+    const postsArray = [];
+    console.log(posts);
+    // posts.node.frontmatter.tags.filter(tagToFind => tags === tagToFind);
+    posts.forEach((post, index) => {
+      const tags = post.node.frontmatter.tags;
+      
+    });
+    console.log(postsArray, 'postsArray');
+    // debugger;
+    
     const postLinks = posts.map((post, index) => {
+      const tagsArray = post.node.frontmatter.tags;
       return (
         <li className='tag-list-item' key={index}>
           <Link className='tag-list-link' to={post.node.frontmatter.path} key={index}>{post.node.frontmatter.title}</Link>
@@ -28,6 +40,7 @@ export const query = graphql`
           frontmatter {
             path
             title
+            tags
           }
         }
       }
