da1fceba345b64548af780f04d31879dc49849ae
aberkow <berkowitz.clarinet@gmail.com>
2018-02-04 20:13:34 -0500

    text edits. need to add resume details at some point

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,8 @@ const About = () =>
   <BlogPostContainer>
     <BlogPostContent>
       <h2>I'm Adam. Nice to meet you!</h2>
-      <p>I'm a web developer based in West Hartford Connecticut. Believe it or not, web development is my second career. Before that, I was a professional musician (performer/educator) for over 10 years. Now, I work full-time for UConn's office of University Communications and I'm loving every minute of it! Every day I get to work on with an amazing (and small) team (2 other devs, 2 designers,and a project manager). Together we work on sites that get seen and used by thousands of people every day.</p>
-      <p>My goal for this site is pretty simple. Because I'm self taught now that I've been working in this field for a year, I wanted to:</p>
+      <p>I'm a web developer based in West Hartford Connecticut. Believe it or not, web development is my second career. Before that, I was a professional musician (performer/educator) for over 10 years. Now, I work full-time for <a href="https://communications.uconn.edu">UConn's office of University Communications</a> and I'm loving every minute of it! Every day I get to work on with an amazing (and small) team (2 other devs, 2 designers,and a project manager). Together we work on sites that get seen and used by thousands of people every day.</p>
+      <p>My goals for this site are pretty simple. Because I'm self taught now that I've been working in this field for a year, I wanted to:</p>
       <ul>
         <li>share what I've learned so far</li>
         <li>keep a record of things I've found interesting and/or challenging</li>
@@ -24,30 +24,39 @@ const About = () =>
             <li>command line tools and techniques</li>
             <li>Docker</li>
             <li>HTML/CSS/SCSS</li>
-            <li>Javascript (e.g. es6, gulp, webpack, react, "vanilla", jquery, etc...</li>
+            <li>Javascript (e.g. es6, gulp, webpack, react, "vanilla", jquery, etc...)</li>
             <li>WordPress and PHP in general</li>
           </ul>
         <li>programming concepts I've learned</li>
         <li>and a lot more!</li>
       </ul>
-      <p>Here are some more lists</p>
-      <p>Things I want to learn/learn more about:</p>
-      <ul>
-        <li>a11y</li>
-        <li>kubernetes</li>
-        <li>vue</li>
-        <li>the web audio API</li>
-      </ul>
-      <p>I'd love to chat, share, and learn more about web development, programming, and music. Feel free to reach out by</p>
+      
+      <strong>I'd love to chat, share, and learn more about web development, programming, and music. Feel free to reach out by</strong>
       <ul>
         <li><a href="mailto:adam@adamjberkowitz.com">email</a></li>
         <li><a href="https://twitter.com/adamjberkowitz">twitter</a></li>
         <li><a href="https://github.com/aberkow">github</a></li>
       </ul>
-      <h2>Resume</h2>
       <h2>A few more things</h2>
+      <h3>Things I want to learn/learn more about:</h3>
+      <ul>
+        <li>a11y</li>
+        <li>graphql</li>
+        <li>kubernetes</li>
+        <li>meteor js</li>
+        <li>vue</li>
+        <li>the web audio API</li>
+      </ul>
+      <h3>More facts about me:</h3>
+      <ul>
+        <li>I've been married for just over 10 years.</li>
+        <li>My wife and I have two sons (ages 8 and 7.5)</li>
+        <li>I grew up in upstate NY (near Rochester)</li>
+        <li>I have a weakness for chocalate chip cookies</li>
+        <li>I have a doctorate of musical arts (DMA) with a specialization in contemporary clarinet and bass clarinet.</li>
+      </ul>
     </BlogPostContent>
   </BlogPostContainer>
   
-
-export default About;
+  export default About;
+  // <h2>Resume</h2>
