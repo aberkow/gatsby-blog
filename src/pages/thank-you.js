4348fd34b8410ee12a7ad4e7acd5929bcb04d555
aberkow <berkowitz.clarinet@gmail.com>
2018-05-27 13:41:10 -0400

    contact form and basic styles

---
+++ src/pages/thank-you.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const ThankYou = () => 
+  <div>
+    <h2>Thanks!</h2>
+    <p>Thanks for writing. I'll get back to you as soon as I can.</p>
+  </div>
+
+export default ThankYou;
